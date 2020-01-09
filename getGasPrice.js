cadcbd3941a3b00618d5fc6bcdcb85b596d36d86
kreid8 <kreid8@kent.edu>
2020-01-09 14:57:44 -0500

    writes down prices

--- getGasPrice.js
+++ getGasPrice.js
@@ -1,15 +1,21 @@
 const http = require('https');
 const xml2js = require('xml2js');
+const fs = require('fs');
+var gasCost = 0;
 const parser = new xml2js.Parser({ attrkey: "ATTR" });
-
+//converts xml to JSON
 let req = http.get("https://www.fueleconomy.gov/ws/rest/fuelprices", function(res) {
     let data = '';
     res.on('data', function(stream) {
         data += stream;
     });
     res.on('end', function(){
+
         parser.parseString(data, function(error, result) {
+		
             if(error === null) {
+				let info = JSON.stringify(result);
+				fs.writeFileSync('gasPrice.json', info);
                 console.log(result);
             }
             else {
