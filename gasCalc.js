69105e2e8339808a6e7edcdf409aab82e7f4c9c6
kreid8 <kreid8@kent.edu>
2020-01-09 15:48:50 -0500

    gets previous gas price

--- gasCalc.js
+++ gasCalc.js
@@ -1,12 +1,27 @@
-var gasPrices = 3.20;
-//var milesToGo = 1000;
-//var mpg = 30;
+var gasPriceCalc = require('./getGasPrice.js');
+const fs = require("fs");
+var gasPrices = gasPriceCalc.getMe();
+let fileName = "gasPrice.json";
+let data = JSON.parse(fs.readFileSync(fileName));
 
+console.log(data);
+console.log(data.fuelPrices.regular);
+gasPrices = data.fuelPrices.regular;
+
+var milesToGo = 1000;
+var mpg = 30;
+
+var gasMoney = milesToGo/mpg;
+gasMoney = gasPrices * gasMoney;
+console.log('gas Price: ' + gasPrices);
 module.exports = {
 	findGasCost: function (milesToGo, mpg) {
+		
 		var gasMoney = milesToGo/mpg;
 		gasMoney = gasPrices * gasMoney;
+		console.log('gas Price: ' + gasPrices);
 		return gasMoney.toString();
+		
 	}
 };
 
--- getGasPrice.js
+++ getGasPrice.js
@@ -3,7 +3,10 @@ const xml2js = require('xml2js');
 const fs = require('fs');
 var gasCost = 0;
 const parser = new xml2js.Parser({ attrkey: "ATTR" });
-//converts xml to JSON
+//converts xml to JSONfind
+module.exports = {
+function getMe()
+{
 let req = http.get("https://www.fueleconomy.gov/ws/rest/fuelprices", function(res) {
     let data = '';
     res.on('data', function(stream) {
@@ -16,7 +19,8 @@ let req = http.get("https://www.fueleconomy.gov/ws/rest/fuelprices", function(re
             if(error === null) {
 				let info = JSON.stringify(result);
 				fs.writeFileSync('gasPrice.json', info);
-                console.log(result);
+                console.log(parseFloat(result.fuelPrices.regular));
+				gasCost = parseFloat(result.fuelPrices.regular);
             }
             else {
                 console.log(error);
@@ -24,3 +28,5 @@ let req = http.get("https://www.fueleconomy.gov/ws/rest/fuelprices", function(re
         });
     });
 });
+return gasCost;
+}
